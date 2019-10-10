07e5fda0cc53b09916434834340b52b91e374c58
Diego Martinez <dmartinezm@gmail.com>
2019-10-10 17:19:30 -0400

    Done.

--- lib/backer.rb
+++ lib/backer.rb
@@ -3,14 +3,19 @@ class Backer
     attr_reader :name, :backed_projects
 
     def initialize(name)
-        @backed_projects = []
+        # create an empty array, we have attr_reader so we can read elements
+        # storing name attribute for this backer
         @name = name
+        @backed_projects = []
     end
 
     def back_project(project)
-        #insert instance of project into array
-        #this instance is passed when it's called 
+        # ^^ project is an instance of Project class
+        # this instance is passed when it's called by backer and stored in array
         @backed_projects << project
+        # this is important! this allows us to access the instance (project) attributes
+        # title and backers[], so we are able to insert into the array <--(self), which is an instance of backer
+        # we are inserting the whole instance object into projects
         project.backers << self
     end
 end
@@ -23,5 +28,5 @@ logan = Backer.new("Logan")
 hoverboard = Project.new("Awesome Hoverboard")
 logan.back_project(hoverboard)
 
-# binding.pry
+binding.pry
 
--- lib/project.rb
+++ lib/project.rb
@@ -1,21 +1,17 @@
 require 'pry'
 class Project
     attr_reader :title, :backers
-    @@projects = []
 
     def initialize(title)
         @title = title
         @backers = []
-        @@projects << self
     end
 
     def add_backer(backer)
         @backers << backer
-        # binding.pry
+        # this is important! this allows us to access the instance (backer) attributes
+        # name and project[], so we are able to insert into the array <--(self), which is an instance of project
+        # we are inserting the whole instance object into backer
         backer.backed_projects << self
-    end
-
-    def self.all
-        @@projects
     end
 end
