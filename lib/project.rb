5a3e541e06f81dfca01744f8deffea730ec8ff1e
Muna Mashhour <munamashhour@gmail.com>
2019-10-10 16:44:17 -0400

    Done.

---
+++ lib/project.rb
@@ -0,0 +1,25 @@
+class Project
+    attr_accessor :title, :backers
+​
+    @@all = [] 
+​
+    def initialize(title)
+        @backers = []
+        @title = title
+        @@all << self 
+    end
+​
+    def add_backer(backer)
+        @backer = backer 
+        backers << backer
+        Backer.all.map do |each_backer|
+            if each_backer == backer 
+                each_backer.backed_projects << self
+            end 
+        end
+    end
+​
+    def self.all
+        @@all
+    end 
+end 
