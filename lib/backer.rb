c7c2c399cacb50c7651e8456880758e79be00a7c
Vannida Lim <vannida.lim@gmail.com>
2019-10-10 16:25:34 -0400

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,26 @@
+class Backer
+    attr_accessor :name, :backed_projects
+
+    @@all = []
+
+    def initialize(name)
+        @backed_projects = []
+        @name = name
+        @@all << self 
+    end
+
+    def back_project(project)
+        @backed_projects << project
+        Project.all.map do |each_project|
+            if each_project == project 
+                each_project.backers << self 
+            end 
+        end 
+    end
+
+    def self.all
+        @@all 
+    end 
+
+end
+
